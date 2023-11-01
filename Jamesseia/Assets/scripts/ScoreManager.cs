ff342af44238273d2067ef07dadd9adbed1ae738
luca-moraes <lcansd.lm@gmail.com>
2023-11-01 03:38:41 -0300

    correcao de nomemclatura

--- Jamesseia/Assets/scripts/ScoreManager.cs
+++ Jamesseia/Assets/scripts/ScoreManager.cs
@@ -2,9 +2,9 @@ using System.Linq;
 using Unity.VisualScripting;
 public static class ScoreManager{
 
-    public static Pontuacao[] pontuacoes = new Pontuacao[6].Select(x => new Pontuacao()).ToArray();
+    public static EstatisticasDeFases[] scoreFases = new EstatisticasDeFases[6].Select(x => new EstatisticasDeFases()).ToArray();
 
     public static void resetPontuacoes(){
-        pontuacoes = new Pontuacao[6].Select(x => new Pontuacao()).ToArray();
+        scoreFases = new EstatisticasDeFases[6].Select(x => new EstatisticasDeFases()).ToArray();
     }
 }
